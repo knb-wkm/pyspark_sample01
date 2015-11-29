4976ae6edd1aa03c434003c6ac0519ddc69afa76
wakayama <y-wakayama@mcc-osaka.co.jp>
2015-11-29 13:57:12 +0900

    見栄えを更にfix

--- static/js/face-change.js
+++ static/js/face-change.js
@@ -21,18 +21,19 @@ $(function(){
     $("#submit").on("click", function(){
         var keywords = $("#keywords").val();
         svg_wrapper.load("/static/images/face_think.svg svg");
+        $("#message").text("考え中...");
         $.ajax({
             type: "POST",
             url:  "/classify",
             data: {"keywords": keywords},
             success: function(data){
                 svg_wrapper.load("/static/images/face_positive.svg svg");                
-                $("#result").text(data);
+                $("#message").text(data);
                 setTimeout(function(){
                     svg_wrapper.load("/static/images/face_normal.svg svg");
+                    $("#message").text("");
                 }, 5000);
             }
         });
     });
-
 });
