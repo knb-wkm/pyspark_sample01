9090f3312c8e0597188e7cf8575641c1b34c8a6b
wakayama <y-wakayama@mcc-osaka.co.jp>
2015-12-02 13:39:28 +0900

    偏差値を算出するメソッドを追加

--- classify.py
+++ classify.py
@@ -5,6 +5,8 @@ from pyspark.mllib.classification import NaiveBayesModel
 from pyspark.mllib.linalg import _convert_to_vector
 import pickle
 import os, sys
+import json
+import numpy
 
 """
 引数に解析させたいキーワードをスペース区切りにて入力
@@ -17,6 +19,11 @@ def likelihood(self, test_tf):
     x = _convert_to_vector(test_tf)
     return self.pi + x.dot(self.theta.transpose())
 
+def standard_score(score):
+    average = numpy.average(score)
+    deviation = numpy.std(score)
+    return 50 + 10 * ((score - average) / deviation)
+
 # mix-in
 NaiveBayesModel.likelihood = likelihood
 
@@ -24,13 +31,8 @@ conf = SparkConf().setAppName("sample").setMaster("local")
 sc = SparkContext(conf=conf)
 
 path = os.path.abspath(os.path.dirname(__file__))
-f = open("%s/model/texts.pick" % path)
-texts = pickle.load(f)
-f.close()
-
-f = open("%s/model/labels.pick" % path)
-labels = pickle.load(f)
-f.close()
+texts = pickle.load(open("%s/model/texts.pick" % path))
+labels = pickle.load(open("%s/model/labels.pick" % path))
 
 texts = sc.parallelize(texts)
 htf = HashingTF(1000)  # Warning!! default value is 2^20
@@ -43,6 +45,8 @@ model = NaiveBayesModel.load(sc, "%s/model" % path)
 test = model.predict(test_tf)
 
 likelihoods = model.likelihood(test_tf)
-print "likelihood: %s" % likelihoods[int(test)]
+print "likelihoods: %s" % likelihoods
+print "standard scores: %s" % standard_score(likelihoods)
 print "label: %s" % labels[int(test)].encode('utf-8')
-
+# json_data = {"likelihood": likelihoods[int(test)], "label": labels[int(test)].encode('utf-8')}
+# print json.dumps(json_data)
--- tfidf_naive_bayes_sample.py
+++ tfidf_naive_bayes_sample.py
@@ -2,32 +2,139 @@ from pyspark import SparkContext, SparkConf
 from pyspark.mllib.feature import HashingTF, IDF
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.mllib.classification import NaiveBayes
+from pyspark.mllib.linalg import _convert_to_vector
 
 def debug_message():
     print "#" * 20 + " debug " + "#" * 20
 
 # conf = SparkConf().setAppName("sample").setMaster("local")
-conf = SparkConf().setAppName("sample").setMaster("spark://172.16.9.3:7077")
+conf = SparkConf().setAppName("sample").setMaster("local")
 sc = SparkContext(conf=conf)
 
 sentence_data = sc.parallelize([
     {"text": "sapporo fukushima tokyo nagoya osaka kobe fukuoka", "label": 0.0},
-    {"text": "alabama alaska arizona california colorado connecticut", "label": 1.0},
-    {"text": "finland france germany greece hungary ireland", "label": 2.0},
+    {"text": "alabama alaska arizona california colorado connecticut", "label": 1.0}
 ])
 
 labels = sentence_data.map(lambda s: s["label"])
+"""
+>>> labels.collect()
+[0.0, 1.0]
+"""
+
 texts = sentence_data.map(lambda s: s["text"].split(' '))
+"""
+>>> texts.collect()
+[['sapporo', 'fukushima', 'tokyo', 'nagoya', 'osaka', 'kobe', 'fukuoka'],
+ ['alabama', 'alaska', 'arizona', 'california', 'colorado', 'connecticut']]
+"""
+
 htf = HashingTF(1000)  # Warning!! default value is 2^20
 tf = htf.transform(texts)
+"""
+>>> type(tf)
+'pyspark.rdd.PipelinedRDD'
+
+>>> tf.collect()
+[SparseVector(1000, {241: 1.0, 293: 1.0, 461: 1.0, 561: 1.0, 576: 1.0, 655: 1.0, 778: 1.0}),
+ SparseVector(1000, {258: 1.0, 261: 1.0, 325: 1.0, 764: 1.0, 859: 1.0, 914: 1.0})]
+"""
 
 idf = IDF().fit(tf)
 tfidf = idf.transform(tf)
+"""
+>>> type(tfidf)
+<class 'pyspark.rdd.RDD'>
+
+>>> tfidf.collect()
+[SparseVector(1000, {241: 0.4055, 293: 0.4055, 461: 0.4055, 561: 0.4055, 576: 0.4055, 655: 0.4055, 778: 0.4055}),
+ SparseVector(1000, {258: 0.4055, 261: 0.4055, 325: 0.4055, 764: 0.4055, 859: 0.4055, 914: 0.4055})]
+"""
 
 training = labels.zip(tfidf).map(lambda x: LabeledPoint(x[0], x[1]))
-model = NaiveBayes.train(training)
+"""
+>>> type(training)
+<class 'pyspark.rdd.PipelinedRDD'>
 
-test_tf = htf.transform(["sapporo", "kobbe"])
+>>> training.collect()
+[LabeledPoint(0.0, (1000,[241,293,461,561,576,655,778],[0.405465108108,0.405465108108,0.405465108108,0.405465108108,0.405465108108,0.405465108108,0.405465108108])),
+ LabeledPoint(1.0, (1000,[258,261,325,764,859,914],[0.405465108108,0.405465108108,0.405465108108,0.405465108108,0.405465108108,0.405465108108]))]
+"""
+
+model = NaiveBayes.train(training)
+"""
+>>> model.pi
+array([-0.69314718, -0.69314718])
+
+>>> model.theta
+array([[-6.91058951, -6.91058951, -6.91058951, ..., -6.91058951,
+        -6.91058951, -6.91058951],
+       [-6.91018512, -6.91018512, -6.91018512, ..., -6.91018512,
+        -6.91018512, -6.91018512]])
+
+>>> model.theta[0][241]
+-6.5702212286920201
+
+>>> model.theta[0][461]
+-6.5702212286920201
+
+>>> model.theta.transpose()
+array([[-6.91058951, -6.91018512],
+       [-6.91058951, -6.91018512],
+       [-6.91058951, -6.91018512],
+       ...,)
+
+>>> model.theta.transpose()[241]
+array([-6.57022123, -6.91018512])
+
+>>> model.theta.transpose()[461]
+array([-6.57022123, -6.91018512])
+
+>>> model.theta.transpose()[0]
+array([-6.91058951, -6.91018512])
+"""
+
+test_tf = htf.transform(["sapporo", "kobe"])
+"""
+>>> test_tf
+SparseVector(1000, {241: 1.0, 461: 1.0})
+
+>>> test_tf.dot(model.theta.transpose())
+array([-13.14044246, -13.82037023])
+
+>>> help(test_tf)
+ |  dot(self, other)
+ |      Dot product with a SparseVector or 1- or 2-dimensional Numpy array.
+ |
+ |      >>> a = SparseVector(4, [1, 3], [3.0, 4.0])
+ |      >>> a.dot(a)
+ |      25.0
+ |      >>> a.dot(array.array('d', [1., 2., 3., 4.]))
+ |      22.0
+ |      >>> b = SparseVector(4, [2, 4], [1.0, 2.0])
+ |      >>> a.dot(b)
+ |      0.0
+ |      >>> a.dot(np.array([[1, 1], [2, 2], [3, 3], [4, 4]]))
+ |      array([ 22.,  22.])
+ |      >>> a.dot([1., 2., 3.])
+ |      Traceback (most recent call last):
+ |          ...
+ |      AssertionError: dimension mismatch
+ |      >>> a.dot(np.array([1., 2.]))
+ |      Traceback (most recent call last):
+ |          ...
+ |      AssertionError: dimension mismatch
+ |      >>> a.dot(DenseVector([1., 2.]))
+ |      Traceback (most recent call last):
+ |          ...
+ |      AssertionError: dimension mismatch
+ |      >>> a.dot(np.zeros((3, 2)))
+ |      Traceback (most recent call last):
+ |          ...
+ |      AssertionError: dimension mismatch
+ |
+"""
+
 test = model.predict(test_tf)
 
 # debug
